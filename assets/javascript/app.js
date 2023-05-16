dbdfdcc24bcd96d2cc950956fd6562937fae5a44
shakirali03 <shakirali.ugc@gmail.com>
2023-05-16 14:52:43 +0530

    client

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,23 @@ setInterval(function () {
   const random = Math.floor(Math.random() * 4);
   image.src = images[random];
 }, 3000);
+
+
+
+// ==================clients=================
+$('.brand-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
